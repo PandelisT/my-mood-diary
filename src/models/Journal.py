bd88ad3c5d20c9266863b09ab8b58aa050f3ef9c
PandelisT <pandeli@nerdypandy.com>
2020-11-09 12:19:55 +1100

    First full crub resource created and working

--- src/models/Journal.py
+++ src/models/Journal.py
@@ -1,8 +1,9 @@
 from main import db
+from datetime import datetime 
 
 class Journal(db.Model):
     __tablename__ = "journal"
 
     id = db.Column(db.Integer, primary_key=True)
     journal_entry = db.Column(db.String())
-    journal_date = db.Column(db.Date.now())
+    journal_date = db.Column(db.DateTime, default=datetime.now)
